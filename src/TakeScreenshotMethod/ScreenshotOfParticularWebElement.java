eda8149badd09bdf5151b382d533e082d34cac6b
hvayandeshkar <hvayandeshkar@KTPCPN-162>
2024-07-29 18:52:05 +0530

    New Commit 29-07-2024

---
+++ src/TakeScreenshotMethod/ScreenshotOfParticularWebElement.java
@@ -0,0 +1,34 @@
+package TakeScreenshotMethod;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.google.common.io.Files;
+
+public class ScreenshotOfParticularWebElement {
+
+	public static void main(String[] args) throws IOException {
+		System.setProperty("webdriver.chrome.driver" ,"./drivers/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+		driver.get("https://www.selenium.dev/downloads/");
+		
+		//screenshot of webelement
+		WebElement target=driver.findElement(By.xpath("//span[.='Downloads']"));
+		File src=target.getScreenshotAs(OutputType.FILE);
+		File dest=new File("./screenshots/seldownload.png");
+		Files.copy(src, dest);
+		
+		
+		;
+	}
+
+}
